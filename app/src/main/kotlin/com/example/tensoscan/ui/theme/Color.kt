dba8597e551d77a51368b28de1ff694f742d30df
kikepb7 <enriquepalmadev@gmail.com>
2025-03-25 00:10:27 +0100

    DATA | Add new resources, colors, strings and dimensions

--- app/src/main/kotlin/com/example/tensoscan/ui/theme/Color.kt
+++ app/src/main/kotlin/com/example/tensoscan/ui/theme/Color.kt
@@ -12,6 +12,11 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+
+val primaryTopBarColor = Color(0xFFFFFFFF)
+val secondaryTopBarColor = Color(0xFF3A3A3A)
+val primaryBackgroundScreenColor = Color(0xFFFFFFFF)
+val secondaryBackgroundScreenColor = Color(0xFF2A2A2A)
 val primaryBackgroundTrackerView = Color(0xFFEDECFD)
 val secondaryBackgroundTrackerView = Color(0xFF1E1E1E)
 val primaryScanDeviceTrackerButtonColor = Color(0xFF6E9BD2)
@@ -20,6 +25,20 @@ val primaryWriteManuallyTrackerButtonColor = Color(0xFF6ED297)
 val secondaryWriteManuallyTrackerButtonColor = Color(0xFFD29D6E)
 val primarySummaryTrackerButtonColor = Color(0xFFAEC4B9)
 val secondarySummaryTrackerButtonColor = Color(0xFF565656)
+val primaryBackgroundSummaryUploadButtonSelector = Color(0xFFEC9D9D)
+val secondaryBackgroundSummaryUploadButtonSelector = Color(0xFF666756)
+val primaryBackgroundSummarySetManuallyButtonSelector = Color(0xFF9DECB7)
+val secondaryBackgroundSummarySetManuallyButtonSelector = Color(0xFF666756)
+val primaryDefaultButtonColor = Color(0xFFFF3543)
+val secondaryDefaultButtonColor = Color(0xFF666756)
+
+val TopBarColor
+    @Composable
+    get() = if (isSystemInDarkTheme()) secondaryTopBarColor else primaryTopBarColor
+
+val BackgroundScreenColor
+    @Composable
+    get() = if (isSystemInDarkTheme()) secondaryBackgroundScreenColor else primaryBackgroundScreenColor
 
 val ScanDeviceButtonTrackerColor
     @Composable
@@ -33,6 +52,19 @@ val SummaryTrackerButtonColor
     @Composable
     get() = if (isSystemInDarkTheme()) secondarySummaryTrackerButtonColor else primarySummaryTrackerButtonColor
 
+val TensoScanDefaultButtonColor
+    @Composable
+    get() = if (isSystemInDarkTheme()) secondaryDefaultButtonColor else primaryDefaultButtonColor
+
 val BackgroundTrackerViewColor
     @Composable
     get() = if (isSystemInDarkTheme()) secondaryBackgroundTrackerView else primaryBackgroundTrackerView
+
+
+val BackgroundSummarySetManuallyButtonSelectorColor
+    @Composable
+    get() = if (isSystemInDarkTheme()) secondaryBackgroundSummarySetManuallyButtonSelector else primaryBackgroundSummarySetManuallyButtonSelector
+
+val BackgroundSummaryUploadButtonSelectorColor
+    @Composable
+    get() = if (isSystemInDarkTheme()) secondaryBackgroundSummaryUploadButtonSelector else primaryBackgroundSummaryUploadButtonSelector
